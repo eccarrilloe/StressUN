6121d3f0803903fd98ebf06ae00a21b70c99a278
Edward Carrillo <eccarrilloe@gmail.com>
2018-06-28 03:40:47 -0500

    feat(constants): keyshortcuts and status added

---
+++ src/stress/constants/KeyShortcuts.java
@@ -0,0 +1,11 @@
+package stress.constants;
+
+public abstract class KeyShortcuts {
+    public static final char AXES = 'x';
+    public static final char COORDINATES = 'c';
+    public static final char TRACKEDOBJECT = 't';
+    public static final char HOVEROBJECT = 'h';
+    public static final char LEVEL = 'l';
+    public static final char FORCES = 'f';
+    public static final char EXTRUDE = 'e';
+}
---
+++ src/stress/constants/Status.java
@@ -0,0 +1,7 @@
+package stress.constants;
+
+public enum Status {
+    MENU,
+    CANVAS,
+    COMMANDLINE
+}
